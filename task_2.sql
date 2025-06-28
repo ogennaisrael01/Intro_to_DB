077f9246891ad2f5a1d83a54a0880fdfb95ef3b6
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-06-28 01:14:25 +0100

    tables for the alx_book_store

---
+++ task_2.sql
@@ -0,0 +1,36 @@
+CREATE TABLE Authors(
+    author_id INT PRIMARY KEY AUTOINCREMENT,
+    author_name VARCHAR(215)
+    ;
+
+CREATE TABLE Books(
+    book_id INT AUTOINCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id  INT,
+    price INT NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY(author_id) REFERENCES authors(author_id)
+    );
+
+CREATE TABLE Customers(
+    customer_id INT PRIMARY KEY AUTOINCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL UNIQUE,
+    address TEXT NOT NULL
+    );
+
+CREATE TABLE Orders(
+    order_id INT PRIMARY KEY AUTOINCREMENT,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+    );
+
+CREATE TABLE Order_Details(
+    order_detail_id INT PRIMARY KEY AUTOINCREMENT,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+    );
