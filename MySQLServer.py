06739a46c2ca3ee15d3016a8d6f080c332eb6f2d
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-06-28 01:05:46 +0100

    creating database for the alx-book-store

---
+++ MySQLServer.py
@@ -0,0 +1,28 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+def connect_db():
+    try:
+        connection = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user="root",
+            password=os.getenv("DB_PASSWORD")
+        )
+        return connection
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+def create_database():
+    try:
+        connection = connect_db()
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        
+        connection.commit()
+        print("Database 'alx_book_store' created successfully!")
+    except Exception as e:
+        print(f"Error creating database: {e}")
+create_database()
